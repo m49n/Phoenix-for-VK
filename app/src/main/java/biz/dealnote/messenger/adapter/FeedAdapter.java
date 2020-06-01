a9624b82c0f115511fe76b49381054a133e5928f
fitsuli <danilyuk_2011@bk.ru>
2020-06-01 21:11:42 +0500

    Remove bottom dividers from posts and improve their appearance a bit

--- app/src/main/java/biz/dealnote/messenger/adapter/FeedAdapter.java
+++ app/src/main/java/biz/dealnote/messenger/adapter/FeedAdapter.java
@@ -61,15 +61,6 @@ public class FeedAdapter extends RecyclerBindableAdapter<News, FeedAdapter.PostH
         return safeAllIsEmpty(news.getAttachments().getPhotos(), news.getAttachments().getVideos());
     }
 
-    private static boolean needToShowBottomDivider(News news) {
-        if (Utils.safeIsEmpty(news.getCopyHistory())) {
-            return news.getAttachments() == null || !news.getAttachments().isPhotosVideosGifsOnly();
-        }
-
-        Post last = news.getCopyHistory().get(news.getCopyHistory().size() - 1);
-        return last.getAttachments() == null || !last.getAttachments().isPhotosVideosGifsOnly();
-    }
-
     @Override
     protected void onBindItemViewHolder(final PostHolder holder, int position, int type) {
         final News item = getItem(position);
@@ -127,7 +118,6 @@ public class FeedAdapter extends RecyclerBindableAdapter<News, FeedAdapter.PostH
         });
 
         holder.topDivider.setVisibility(View.GONE);
-        holder.bottomDivider.setVisibility(needToShowBottomDivider(item) ? View.VISIBLE : View.GONE);
 
         holder.viewsCounterRoot.setVisibility(item.getViewCount() > 0 ? View.VISIBLE : View.GONE);
         //holder.viewsCounter.setText(String.valueOf(item.getViewCount()));
@@ -213,7 +203,6 @@ public class FeedAdapter extends RecyclerBindableAdapter<News, FeedAdapter.PostH
         TextView tvText;
         TextView tvShowMore;
         TextView tvTime;
-        View bottomDivider;
         ViewGroup bottomActionsContainer;
         CircleCounterButton likeButton;
         CircleCounterButton shareButton;
@@ -235,7 +224,6 @@ public class FeedAdapter extends RecyclerBindableAdapter<News, FeedAdapter.PostH
             tvText = root.findViewById(R.id.item_post_text);
             tvShowMore = root.findViewById(R.id.item_post_show_more);
             tvTime = root.findViewById(R.id.item_post_time);
-            bottomDivider = root.findViewById(R.id.bottom_divider);
             bottomActionsContainer = root.findViewById(R.id.buttons_bar);
             likeButton = root.findViewById(R.id.like_button);
             commentsButton = root.findViewById(R.id.comments_button);
