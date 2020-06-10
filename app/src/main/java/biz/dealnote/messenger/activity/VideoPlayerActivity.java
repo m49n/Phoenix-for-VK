eabd039d4630974fa322220e1034b30c2f2a1ebe
fitsuli <danilyuk_2011@bk.ru>
2020-06-11 00:51:35 +0500

    Change fonts a little and fix difficult escaping from video player with back button

--- app/src/main/java/biz/dealnote/messenger/activity/VideoPlayerActivity.java
+++ app/src/main/java/biz/dealnote/messenger/activity/VideoPlayerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -15,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -153,11 +155,12 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
             mControllerView.show();
         }
         mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+            mDecorView.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES));
     }
 
     @Override
@@ -175,6 +178,9 @@ public class VideoPlayerActivity extends AppCompatActivity implements SurfaceHol
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+            mDecorView.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES));
     }
 
     @Override
